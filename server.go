02e072f7bd8d70f89a888f7e7e0fc446a750e621
Oskar Wickström <oskar.wickstrom@live.com>
2015-11-24 21:59:30 +0100

    Add basic skeleton for editor

--- server.go
+++ server.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/oden-lang/playground/Godeps/_workspace/src/github.com/codegangsta/negroni"
+	"github.com/oden-lang/playground/Godeps/_workspace/src/github.com/unrolled/render"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello")
+type ViewModel struct {
+	OdenSource    string
+	GoOutput      string
+	ConsoleOutput string
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	r := render.New(render.Options{
+		Layout:     "layout",
+		Extensions: []string{".html"},
+	})
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		r.HTML(w, http.StatusOK, "index", ViewModel{
+			"(pkg main)",
+			"package main",
+			"",
+		})
+	})
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-	fmt.Println("Starting server at port " + port)
-	http.ListenAndServe(":"+port, nil)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Use(negroni.NewStatic(http.Dir("public")))
+	n.Run(":" + port)
 }
