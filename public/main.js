9cdc0c0b4d5ba0788d8215bca7c9a5779f69ae29
Oskar Wickström <oskar.wickstrom@live.com>
2015-11-26 06:52:21 +0100

    Codemirror!

--- public/main.js
+++ public/main.js
@@ -1,1 +1,14 @@
-document.querySelector('textarea').select();
+//document.querySelector('textarea').select();
+
+var sourceTextArea = document.querySelector('.editor .source textarea');
+var sourceCodeMirror = CodeMirror.fromTextArea(sourceTextArea, {
+  mode: 'clojure',
+  title: 'Oden source code'
+});
+
+var outputTextArea = document.querySelector('.editor .output textarea');
+var outputCodeMirror = CodeMirror.fromTextArea(outputTextArea, {
+  mode: 'go',
+  readOnly: true,
+  title: 'Go output'
+});
